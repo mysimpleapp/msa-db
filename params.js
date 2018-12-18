cc34a9d27d71dc456793e0e914272b37c84837cb
Nicolas CARREZ <nicolas.carrez@gmail.com>
2018-12-18 23:07:55 +0100

    Migrate to ParamDef

--- params.js
+++ params.js
@@ -1,23 +1,21 @@
 const { join } = require('path')
 
-Msa.registerParam({
-	"db.type": {
-		desc: "Database type",
-		choices: [ "sqlite", "mysql", "postgre" ]
-	},
-	"db.path": {
-		desc: "Path to DB file (sqlite only)",
-	},
-	"db.pool.min": {
-		defVal: 0
-	},
-	"db.pool.max": {
-		defVal: 5
-	},
-	"db.pool.acquire": {
-		defVal: 10000
-	},
-	"db.pool.idle": {
-		defVal: 30000
-	},
+new Msa.ParamDef("db.type", {
+	desc: "Database type",
+	choices: [ "sqlite", "mysql", "postgre" ]
+})
+new Msa.ParamDef("db.path", {
+	desc: "Path to DB file (sqlite only)"
+})
+new Msa.ParamDef("db.pool.min", {
+	defVal: 0
+})
+new Msa.ParamDef("db.pool.max", {
+	defVal: 5
+})
+new Msa.ParamDef("db.pool.acquire", {
+	defVal: 10000
+})
+new Msa.ParamDef("db.pool.idle", {
+	defVal: 30000
 })
