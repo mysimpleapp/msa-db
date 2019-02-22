59babacb87563978a10418d111de0638843e8068
Nicoco220983 <nicolas.carrez@gmail.com>
2019-02-22 22:59:55 +0100

    Simplified msa_start & msa_install

--- msa_install.js
+++ msa_install.js
@@ -1,20 +1,17 @@
 require('./params.js')
 
-module.exports = async (itf, next) => {
-	try {
+module.exports = async itf => {
+	await itf.questionParam({
+		key: "db.type",
+		question: "Which db type do you choose ?",
+		defVal: "sqlite"
+	})
+	if(Msa.params.db.type === "sqlite"){
 		await itf.questionParam({
-			key: "db.type",
-			question: "Which db type do you choose ?",
-			defVal: "sqlite"
+			key: "db.path",
+			question: "Choose path to DB file",
+			defVal: "db.sqlite"
 		})
-		if(Msa.params.db.type === "sqlite"){
-			await itf.questionParam({
-				key: "db.path",
-				question: "Choose path to DB file",
-				defVal: "db.sqlite"
-			})
-			await itf.install("sqlite3", { dir:__dirname })
-		}
-	} catch(err) { return next(err) }
-	next()
+		await itf.install("sqlite3", { dir:__dirname })
+	}
 }
